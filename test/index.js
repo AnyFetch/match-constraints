16b7abe9fde1db49212a7ca6f59d9df72b11a3c2
Neamar <neamar@neamar.fr>
2014-02-19 14:05:07 +0100

    $match match

--- test/index.js
+++ test/index.js
@@ -3,7 +3,27 @@
 require("should");
 
 var lib = require('../lib');
+var matchConstraints = lib.matchConstraints;
 var matchConstraintsBool = lib.matchConstraintsBool;
+
+
+// Return true, or the error if any.
+function boolWrapper(constraints, object) {
+  var r = matchConstraintsBool(constraints, object);
+  if(r) {
+    return r;
+  }
+
+  try {
+    matchConstraints(constraints, object);
+  }
+  catch(e) {
+    return e.toString();
+  }
+
+  throw new Error("No reason to reach this point.");
+}
+
 
 describe("Match objects checker", function() {
   it("should check for property value", function() {
@@ -11,20 +31,33 @@ describe("Match objects checker", function() {
       name: "hello"
     };
 
-    matchConstraintsBool(constraints, {name: "hello"}).should.be.true;
-    matchConstraintsBool(constraints, {name: "hllo"}).should.be.false;
-    matchConstraintsBool(constraints, {}).should.be.false;
+    boolWrapper(constraints, {name: "hello"}).should.be.true;
+    boolWrapper(constraints, {name: "hllo"}).should.not.be.true;
+    boolWrapper(constraints, {}).should.not.be.true;
   });
 
   it("should check for $contains", function() {
     var constraints = {
       values: {
-        "$contains": "hello"
+        $contains: "hello"
       }
     };
 
-    matchConstraintsBool(constraints, {values: ['hi', 'hello', 'bonjour']}).should.be.true;
-    matchConstraintsBool(constraints, {values: "hello"}).should.be.true;
-    matchConstraintsBool(constraints, {values: ['hi']}).should.be.false;
+    boolWrapper(constraints, {values: ['hi', 'hello', 'bonjour']}).should.be.true;
+    boolWrapper(constraints, {values: "hello"}).should.be.true;
+    boolWrapper(constraints, {values: ['hi']}).should.not.be.true;
+    boolWrapper(constraints, {}).should.not.be.true;
+  });
+
+  it("should check for $match", function() {
+    var constraints = {
+      values: {
+        $match: /h.+o/
+      }
+    };
+
+    boolWrapper(constraints, {values: "hello"}).should.be.true;
+    boolWrapper(constraints, {values: "lol"}).should.not.be.true;
+    boolWrapper(constraints, {}).should.not.be.true;
   });
 });
